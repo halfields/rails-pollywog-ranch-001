fe3a32bb4e01e2275eb1b57f685d07eea771c3f9
hal_fields <hfields697@aol.com>
2016-11-27 19:48:33 -0600

    7:48pm Nov 27, 2016

--- app/models/frog.rb
+++ app/models/frog.rb
@@ -2,4 +2,11 @@ class Frog < ActiveRecord::Base
   # code goes here
   has_many :tadpoles
   belongs_to :pond
+
+  attr_accessor :name, :color
+
+ # def initialize(name, color)
+  #  @name = name
+ #   @color = color
+ # end
 end
--- app/models/pond.rb
+++ app/models/pond.rb
@@ -1,5 +1,11 @@
 class Pond < ActiveRecord::Base
   # code goes here
   has_many :frogs
-  has_many :tadpoles
+  has_many :tadpoles, through: :frogs
+  attr_accessor :name, :water_type
+
+ # def initialize(name, water_type)
+  #  @name = name
+  #  @water_type = water_type
+#  end
 end
